b9b9d77ade30eaa75cde91f50ab2d40007e9246b
Mohsen_Shiekhy <151166968+Kealos5@users.noreply.github.com>
2023-12-29 20:05:28 +0330

    Add files via upload

---
+++ Untitled-1.php
@@ -0,0 +1,50 @@
+<?php
+
+    //test project 1
+
+    // Variable - (str-float-int-bo0lean-array)
+
+    $NUm_1 = 10;
+    $Num_2 = 5;
+    // echo $NUm_1 + $Num_2;
+    echo $NUm_1."</br>";
+    unset ($NUm_1);
+    echo $Num_1."</br>";
+
+
+
+    // Array
+    
+    $My_array = array("Medisa","Javadi Rad","Female",20,19.75,true);
+    // echo $My_array[0];
+    // var_dump($My_array);
+    // print_r($My_array);
+
+    $My_boolean = true;
+    $My_boolean_2 = false;
+
+    // echo gettype($My_boolean)."</br>";
+    // echo gettype($My_boolean_2)."</br>";
+    // settype($My_boolean_2,'string')."</br>";
+    // echo $My_boolean_2;
+
+    // Html - PHP
+    $My_str = "Hello World";
+    $My_h2_start = "<h2 style='color:Red;' >";
+    $My_h2_end = "<h2>";
+    echo $My_h2_start.$My_str.$My_h2_end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
